ba542fbc0882366bee9e5e88233a0ba855ac44a2
ngugikubai <kubaibrian@gmail.com>
2022-03-14 02:19:04 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+/**
+ *main - main function
+ *Description : prints lowercase and uppercase alphabets using putchar
+ *Return: 0
+ */
+int main(void)
+{
+char alphabet;
+char x = '\n'
+for(alphabet ='a'; alphabet <='z'; ++alphabet)
+{
+putchar(alphabet);
+}
+for(alphabet ='A'; alphabet <='Z'; ++alphabet)
+{
+putchar(alphabet);
+}
+putchar(x);
+return (0);
+}
