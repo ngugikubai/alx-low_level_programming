bbecc032578d5827f947318f83081513ceff4c6b
ngugikubai <kubaibrian@gmail.com>
2022-03-14 20:27:52 -0700

    Patience, persistence and perspiration make an unbeatable combination for success

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,18 +9,15 @@
 int main(void)
 {
 int num;
-char x = '\n';
-char z = 44;
-char y = 32;
 for (num = 0; num < 10; num++)
 {
 putchar(num + '0');
 if (num < 9)
 {
-putchar(z);
-putchar(y);
+putchar(44);
+putchar(32);
 }
 }
-putchar(x);
+putchar('\n');
 return (0);
 }
