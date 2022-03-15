1b7f5c1dace06782801fe1b80bc2d082620e9160
ngugikubai <kubaibrian@gmail.com>
2022-03-14 20:01:22 -0700

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * Description -a program that prints the lowercase alphabet in reverse using putchar
+ * Return: 0
+ */
+int main(void)
+{
+char alphabet;
+char x = '\n';
+for (alphabet = 'z'; alphabet >= 'a'; --alphabet)
+{
+putchar(alphabet);
+}
+putchar(x);
+return (0);
