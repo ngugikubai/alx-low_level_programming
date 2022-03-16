df21d3f38ae10a1b558959e739cc1eda3ced6cd2
ngugikubai <kubaibrian@gmail.com>
2022-03-16 20:57:36 +0300

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -12,13 +12,11 @@ int main(void)
 {
     char c[] = "_putchar";
     int i = 0;
-
     while (c[i] != '\0')
     {
         _putchar(c[i]);
         i++;
     }
     _putchar(10);
-
     return (0);
 }
