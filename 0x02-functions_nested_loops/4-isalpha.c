146d8a91d09d15b233620639db41dfb80f2cdd60
ngugikubai <kubaibrian@gmail.com>
2022-03-17 00:15:49 +0300

    isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -8,12 +8,12 @@
 
 int _isalpha(int c)
 {
-    if ((c >= 97 && c <= 123) || (c >= 65 && c <= 91))
-    {
-        return (1);
-    }
-    else
-    {
-        return (0);
-    }
+if ((c >= 97 && c <= 123) || (c >= 65 && c <= 91))
+{
+return (1);
+}
+else
+{
+return (0);
+}
 }
