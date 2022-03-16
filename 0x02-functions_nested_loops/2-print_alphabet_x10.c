94cb735f5bf9a276d255bc4948b64230809487da
ngugikubai <kubaibrian@gmail.com>
2022-03-16 23:46:45 +0300

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,15 +8,15 @@
 
 void print_alphabet_x10(void)
 {
-	char s = 'a';
-	int i, f;
+char s = 'a';
+int i, f;
 
-	for (i = 0; i < 10; i++)
-	{
-		for (f = 0; f < 26; f++)
-		{
-			_putchar(s + f);
-		}
-		_putchar(10);
-	}
+for (i = 0; i < 10; i++)
+{
+for (f = 0; f < 26; f++)
+{
+_putchar(s + f);
+}
+_putchar(10);
+}
 }
