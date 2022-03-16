428451c10de5a7a25ee7f502fff83f0620e42cab
ngugikubai <kubaibrian@gmail.com>
2022-03-16 21:00:48 +0300

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,22 @@
 #include "main.h"
 
 /**
- * main - main function
- * Description: prints _putchar using a custom header
- * The main.h file has a prototype function called _putchar
- * that is linked to a file called _putchar.c
- * Return: 0
+ * main - prints _printchar and new line
+ *
+ * Return: return 0 if successful
  */
 
 int main(void)
 {
     char c[] = "_putchar";
     int i = 0;
+
     while (c[i] != '\0')
     {
         _putchar(c[i]);
         i++;
     }
     _putchar(10);
+
     return (0);
 }
